c336640f5bbd39ea464cd238fade3296803268aa
Jose Lazaro <lazaro0089@gmail.com>
2020-10-03 12:11:04 +0200

    spoty

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http: HttpClient) {
+    console.log('Spotify Service Listo');
+  }
+
+  getQuery( query: string ) {
+
+    const url = `https://api.spotify.com/v1/${ query }`;
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQCi1Jp3nQtPKTqg7u-_Td9UBG2TqKvsaa2YbLQpXhkWanwfAHFQKJ4lf3qJg36fGZpaP6_OfxHHzfpqeuk'
+    });
+
+    return this.http.get(url, { headers });
+
+  }
+
+
+  getNewReleases() {
+
+    return this.getQuery('browse/new-releases?limit=20')
+              .pipe( map( data => data['albums'].items ));
+
+  }
+
+  getArtistas( termino: string ) {
+
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+                .pipe( map( data => data['artists'].items));
+
+  }
+
+  getArtista( id: string ) {
+
+    return this.getQuery(`artists/${ id }`);
+                // .pipe( map( data => data['artists'].items));
+
+  }
+
+  getTopTracks( id: string ) {
+
+    return this.getQuery(`artists/${ id }/top-tracks?country=us`)
+                .pipe( map( data => data['tracks']));
+
+  }
+
+}
